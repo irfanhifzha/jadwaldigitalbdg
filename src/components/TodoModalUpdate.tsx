5c0ca49165e3090579ab721910b76552cd06178f
irfanhifzha <irfan.ulya@digitalbdg.ac.id>
2026-07-21 09:43:52 +0700

    ...

--- src/components/TodoModalUpdate.tsx
+++ src/components/TodoModalUpdate.tsx
@@ -444,7 +444,7 @@ export default function TodoModal({ open, onClose, data }: any) {
                         </div>
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-4">
                             <div>
-                                <label htmlFor="progressTarget">✒️ Target Progress</label>
+                                <label htmlFor="progressTarget">🎯 Target Progress</label>
 
                                 <input
                                     id="progressTarget"
@@ -456,14 +456,20 @@ export default function TodoModal({ open, onClose, data }: any) {
                                     }
                                     onChange={(e) => setProgressTargetLocal(e.target.value)}
                                 />
-                                <p className="text-xs text-gray-500 mb-1">
-                                    Tap to select a date and time.
-                                </p>
+                                {data.task?.status === "todo" ? (
+                                    <p className="text-xs text-gray-500 mb-1">
+                                        Tap to select a date and time.
+                                    </p>
+                                ) : (
+                                    <p className="text-xs text-gray-500 mb-1">
+                                        Tidak bisa diubah
+                                    </p>
+                                )}
                             </div>
 
 
                             <div>
-                                <label htmlFor="doneTarget">✒️ Target Done</label>
+                                <label htmlFor="doneTarget">🎯 Target Done</label>
 
                                 <input
                                     id="doneTarget"
@@ -501,7 +507,7 @@ export default function TodoModal({ open, onClose, data }: any) {
 
                         <div className="grid grid-cols-2 gap-4 mt-2">
                             <div>
-                                <label>✒️ Target Progress</label>
+                                <label>🎯 Target Progress</label>
                                 <div className="mt-1 py-1">
                                     <p className="text-gray-400 text-xs">
                                         {data?.task.progressTarget ? (() => {
@@ -521,7 +527,7 @@ export default function TodoModal({ open, onClose, data }: any) {
                             </div>
 
                             <div>
-                                <label>✒️ Target Done</label>
+                                <label>🎯 Target Done</label>
                                 <div className="mt-1 py-1">
                                     <p className="text-gray-400 text-xs">
                                         {data?.task.doneTarget ? (() => {
